3a382ca04b28da812853e930afe5f4d6421a7ae5
LorenaGalvaoGomes <lory.galvao@hotmail.com>
2015-03-19 19:47:08 -0300

    teste

--- Atividade_01/15.cpp
+++ Atividade_01/15.cpp
@@ -1,4 +1,19 @@
 //15­ Faça um programa que receba o salário de um funcionário e o percentual de aumento, calcule e mostre o valor
 //do aumento e o novo salário.
 
+#include <stdio.h>
 
+int sal(){
+	float sal,aum,porc;
+	scanf("%f %f",&sal,&porc);
+	aum=sal*porc;
+	sal=sal+aum;
+	printf("%f",sal);
+	
+	
+}
+
+main(){
+	sal();
+	
+}
--- Atividade_01/18.cpp
+++ Atividade_01/18.cpp
@@ -1,1 +1,17 @@
 //18 ­Faça um programa que calcule e mostre a área de um triângulo. Supondo que: Área = (base *altura) / 2.
+
+
+#include <stdio.h>
+
+int areaTriangulo(){
+   float base,altura,area;
+   scanf("%f %f",&base,&altura);
+   altura=base*altura;
+   printf("%f\n",altura);
+	
+}
+
+main(){
+	areaTriangulo();
+	
+}
