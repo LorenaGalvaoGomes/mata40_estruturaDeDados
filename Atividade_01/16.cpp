2579fa392573c6f6250d1ff38ae31ab57128ec5b
LorenaGalvaoGomes <lory.galvao@hotmail.com>
2015-03-19 19:47:20 -0300

    Revert "teste"
    This reverts commit 3a382ca04b28da812853e930afe5f4d6421a7ae5.

--- Atividade_01/16.cpp
+++ Atividade_01/16.cpp
@@ -1,17 +1,4 @@
 //16­ Faça um programa que receba o salário base de um funcionário, calcule e mostre o salário a receber,
 //sabendo­se que o funcionário tem gratificação de 5% sobre o salário base e paga imposto de 7% sobre este salário.
 
-#include <stdio.h>
-float salario(float a){
-	float newsal = (a * 1.05)*0.93;
-	return newsal;
-}
-
-int main(){
-	float x;
-	puts("Digite o salario base!");
-	scanf("%f" , &x);
-	printf("O seu salario e %.2f reais ", salario(x));
-	
-}
 
--- Atividade_01/17.cpp
+++ Atividade_01/17.cpp
@@ -1,25 +1,3 @@
 //17­ Faça um programa que receba o salário base de um funcionário, calcule e mostre o seu salário a receber,
 //sabendo­se que o funcionário tem gratificação de R$ 50 e paga imposto de 10% sobre o salário base.
 
-#include <stdio.h> 
-#include <math.h> 
-#include <stdlib.h>
-
-float CalculaSalario(float salario);
-
-int main()
-{
-	float sal,vlrSalario;
-	printf("Informe o valor do seu salario:\n ");
-	scanf("%f", &sal);
-	vlrSalario  = CalculaSalario(sal);
-	printf("O valor do seu novo salario eh %.2f", vlrSalario);	
-}
-float CalculaSalario(float salario)
-{
-   	float valorNovoSalario, gratificacao, imposto;
-   	gratificacao = 50.00;
-   	imposto = salario*0.10;   	
-	valorNovoSalario = salario + gratificacao - imposto;
-	return valorNovoSalario;
-}
