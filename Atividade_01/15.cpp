5ce60b6eceb4d6ad45793b682a8719b412aec29e
LorenaGalvaoGomes <lory.galvao@hotmail.com>
2015-03-19 19:47:29 -0300

    Revert "atividade_01"
    This reverts commit 6ba037fdce0742986c5d87cb25f57e00b1ad10e1.

--- Atividade_01/15.cpp
+++
@@ -1,4 +0,0 @@
-//15­ Faça um programa que receba o salário de um funcionário e o percentual de aumento, calcule e mostre o valor
-//do aumento e o novo salário.
-
-
--- Atividade_01/18.cpp
+++
@@ -1,1 +0,0 @@
-//18 ­Faça um programa que calcule e mostre a área de um triângulo. Supondo que: Área = (base *altura) / 2.
